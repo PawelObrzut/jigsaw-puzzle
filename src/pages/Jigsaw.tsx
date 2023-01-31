fe9bcc372339867202f9a3cfde09b0b5a4becc94
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-31 10:46:03 +0100

    adds jigsaw component

--- src/pages/Jigsaw.tsx
+++ src/pages/Jigsaw.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { JigsawPuzzle } from 'react-jigsaw-puzzle/lib'
 
 const Jigsaw = () => {
   const navigate = useNavigate();
@@ -10,8 +11,22 @@ const Jigsaw = () => {
       <section className="p-2">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => navigate(-1)}>Back to homePage</button>
       </section>
-      <section className="p-2">
+
+      <section className="border-2 m-2 grid grid-rows-2 grid-cols-4 grid-flow-col">
         <img className="h-48 w-48" src={require(`../images/${jigsaw}.png`)} alt={jigsaw} />
+        <div className="border-2 p-2 h-48 w-48">
+          clock
+        </div>
+        <div className="border-2 p-2 h-48 w-48">
+          another component I do not know what for
+        </div>
+        <div className="border-2 h-480 w-480 row-span-3 col-span-3">
+          <JigsawPuzzle 
+            imageSrc={require(`../images/${jigsaw}.png`)}
+            rows={4}
+            columns={6}
+          />
+        </div>
       </section>
     </>
   )
