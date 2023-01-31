a6f8d756f5f82899e736f9f5d5e65c7ef121ee30
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-31 20:32:23 +0100

    adds new game button

--- src/pages/Jigsaw.tsx
+++ src/pages/Jigsaw.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { JigsawPuzzle } from 'react-jigsaw-puzzle/lib';
 
@@ -7,17 +8,30 @@ import Clock from '../components/Clock';
 const Jigsaw = () => {
   const navigate = useNavigate();
   const { jigsaw } = useParams();
+  const [grid, setGrid] = useState({
+    rows: 5,
+    columns: 4
+  })
 
   const handleSolved = () => {
-    // save time to localStorage
-    // play a sound
+    setGrid( {
+      rows: 1,
+      columns: 1
+    });
+  }
+
+  const handleNewGame = () => {
+    setGrid( {
+      rows: 5,
+      columns: 4
+    });
   }
 
   return (
     <>
       <section className="p-2 mx-auto text-center">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-3 rounded" onClick={() => navigate(-1)}>Back to homePage</button>
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-3 rounded">New Game</button>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-3 rounded" onClick={handleNewGame}>New Game</button>
       </section>
 
       <section className="border-2 mb-5 mx-auto grid grid-rows-2 grid-cols-4 grid-flow-col min-w-3xl max-w-4xl">
@@ -32,8 +46,8 @@ const Jigsaw = () => {
         <div className="border-l-2 h-480 w-480 row-span-3 col-span-3">
           <JigsawPuzzle 
             imageSrc={require(`../images/${jigsaw}.png`)}
-            rows={5}
-            columns={4}
+            rows={grid.rows}
+            columns={grid.columns}
             onSolved={handleSolved}
           />
         </div>
